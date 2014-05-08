2fc373fce947bef2dfccedaa748384aae6d6e95c
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2014-05-08 16:11:43 +0200

    Add cynara directory structure, manifest and build files
    Definition of libcynara-client header
    
    [Bug/Feature]   N/A
    [Cause]         Definition of repository and build structure.
    [Solution]      Directory structure and build system files added.
    [Verification]  Should build on all platforms and provide following
                    packages:cynara, libcynara-admin, libcynara-client.
    
    Change-Id: I922c2e0ccce5b0e49302aa643afbc2fbb9778cd2
    Signed-off-by: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>

---
+++ packaging/cynara.spec
@@ -0,0 +1,170 @@
+Name:       cynara
+Summary:    Cynara service with client libraries
+Version:    0.0.1
+Release:    1
+Group:      Security/Access Control
+License:    Apache-2.0
+Source0:    %{name}-%{version}.tar.gz
+Source1001:    cynara.manifest
+Source1002:    libcynara-client.manifest
+Source1003:    libcynara-admin.manifest
+BuildRequires: cmake
+BuildRequires: zip
+BuildRequires: pkgconfig(libsystemd-daemon)
+%{?systemd_requires}
+
+%description
+service and client libraries (libcynara-client, libcynara-admin)
+
+#######################################################
+%package -n libcynara-client
+Summary:    Cynara - client library
+Requires:   cynara = %{version}-%{release}
+Requires(post): /sbin/ldconfig
+Requires(postun): /sbin/ldconfig
+
+%description -n libcynara-client
+client library for checking policies
+
+%package -n libcynara-client-devel
+Summary:    Cynara - client library (devel)
+Requires:   libcynara-client = %{version}-%{release}
+
+%description -n libcynara-client-devel
+client library (devel) for checking policies
+
+#######################################################
+%package -n libcynara-admin
+Summary:    Cynara - admin client library
+Requires:   cynara = %{version}-%{release}
+Requires(post): /sbin/ldconfig
+Requires(postun): /sbin/ldconfig
+
+%description -n libcynara-admin
+admin client library for setting, listing and removing policies
+
+%package -n libcynara-admin-devel
+Summary:    Cynara - admin client library (devel)
+Requires:   libcynara-admin = %{version}-%{release}
+
+%description -n libcynara-admin-devel
+admin client library (devel) for setting, listing and removing policies
+
+#######################################################
+%package -n cynara-devel
+Summary:    Cynara service (devel)
+Requires:   cynara = %{version}-%{release}
+
+%description -n cynara-devel
+service (devel version)
+
+
+%prep
+%setup -q
+cp -a %{SOURCE1001} .
+cp -a %{SOURCE1002} .
+cp -a %{SOURCE1003} .
+
+%build
+%if 0%{?sec_build_binary_debug_enable}
+export CFLAGS="$CFLAGS -DTIZEN_DEBUG_ENABLE"
+export CXXFLAGS="$CXXFLAGS -DTIZEN_DEBUG_ENABLE"
+export FFLAGS="$FFLAGS -DTIZEN_DEBUG_ENABLE"
+%endif
+
+export LDFLAGS+="-Wl,--rpath=%{_libdir}"
+
+%cmake . -DVERSION=%{version} \
+        -DCMAKE_BUILD_TYPE=%{?build_type:%build_type}%{!?build_type:RELEASE} \
+        -DCMAKE_VERBOSE_MAKEFILE=ON
+make %{?jobs:-j%jobs}
+
+%install
+rm -rf %{buildroot}
+%make_install
+
+mkdir -p %{buildroot}/usr/lib/systemd/system/multi-user.target.wants
+ln -s ../cynara.service %{buildroot}/usr/lib/systemd/system/multi-user.target.wants/cynara.service
+mkdir -p %{buildroot}/usr/lib/systemd/system/sockets.target.wants
+ln -s ../cynara.socket %{buildroot}/usr/lib/systemd/system/sockets.target.wants/cynara.socket
+ln -s ../cynara-admin.socket %{buildroot}/usr/lib/systemd/system/sockets.target.wants/cynara-admin.socket
+
+%post
+systemctl daemon-reload
+if [ $1 = 1 ]; then
+    # installation
+    systemctl start cynara.service
+fi
+
+if [ $1 = 2 ]; then
+    # update
+    systemctl restart cynara.service
+fi
+
+/sbin/ldconfig
+
+%preun
+if [ $1 = 0 ]; then
+    # unistall
+    systemctl stop cynara.service
+fi
+
+%postun
+if [ $1 = 0 ]; then
+    # unistall
+    systemctl daemon-reload
+fi
+/sbin/ldconfig
+
+%post -n libcynara-client -p /sbin/ldconfig
+
+%postun -n libcynara-client -p /sbin/ldconfig
+
+%post -n libcynara-admin -p /sbin/ldconfig
+
+%postun -n libcynara-admin -p /sbin/ldconfig
+
+%post -n libcynara-client-devel -p /sbin/ldconfig
+
+%postun -n libcynara-client-devel -p /sbin/ldconfig
+
+%post -n libcynara-admin-devel -p /sbin/ldconfig
+
+%postun -n libcynara-admin-devel -p /sbin/ldconfig
+
+%files -n cynara
+%manifest cynara.manifest
+%license LICENSE
+%attr(755,root,root) /usr/bin/cynara
+%{_libdir}/libcynara-commons.so*
+%attr(-,root,root) /usr/lib/systemd/system/multi-user.target.wants/cynara.service
+%attr(-,root,root) /usr/lib/systemd/system/cynara.service
+%attr(-,root,root) /usr/lib/systemd/system/cynara.target
+%attr(-,root,root) /usr/lib/systemd/system/sockets.target.wants/cynara.socket
+%attr(-,root,root) /usr/lib/systemd/system/cynara.socket
+%attr(-,root,root) /usr/lib/systemd/system/sockets.target.wants/cynara-admin.socket
+%attr(-,root,root) /usr/lib/systemd/system/cynara-admin.socket
+
+%files -n libcynara-client
+%manifest libcynara-client.manifest
+%license LICENSE
+%defattr(-,root,root,-)
+%{_libdir}/libcynara-client.so.*
+
+%files -n libcynara-client-devel
+%defattr(-,root,root,-)
+%{_includedir}/cynara/cynara-client.h
+%{_libdir}/pkgconfig/cynara-client.pc
+%{_libdir}/libcynara-client.so
+
+%files -n libcynara-admin
+%manifest libcynara-admin.manifest
+%license LICENSE
+%defattr(-,root,root,-)
+%{_libdir}/libcynara-admin.so.*
+
+%files -n libcynara-admin-devel
+%defattr(-,root,root,-)
+%{_includedir}/cynara/cynara-admin.h
+%{_libdir}/pkgconfig/cynara-admin.pc
+%{_libdir}/libcynara-admin.so
---
+++ src/include/cynara-client.h
@@ -0,0 +1,168 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        cynara-client.h
+ * @author      Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
+ * @version     1.0
+ * @brief       This file contains client APIs of Cynara available with libcynara-client.
+ */
+
+
+#ifndef CYNARA_CLIENT_H
+#define CYNARA_CLIENT_H
+
+/**
+ * \name Return Codes
+ * exported by the foundation API.
+ * result codes begin with the start error code and extend into negative direction.
+ * @{
+*/
+
+enum cynara_api_result
+{
+/*! \brief   indicating the result of the one specific API is successful or access is allowed */
+	CYNARA_API_SUCCESS,
+
+/*! \brief   indicating that access that was checked is denied */
+	CYNARA_API_ACCESS_DENIED,
+
+/*! \brief   indicating system is running out of memory state */
+	CYNARA_API_OUT_OF_MEMORY,
+
+/*! \brief   indicating the API's parameter is malformed */
+	CYNARA_API_INVALID_PARAM
+};
+/** @}*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct cynara cynara;
+typedef struct cynara_configuration cynara_configuration;
+
+
+/**
+ * \par Description:
+ * Initialize cynara-client library with given configuration.
+ * Create structured used in following API calls.
+ *
+ * \par Purpose:
+ * This API must be used by prior calling cynara_check function.
+ *
+ * \par Typical use case:
+ * Once before a service can call cynara_check.
+ *
+ * \par Method of function operation:
+ * This API initializes inner library structures [TODO describe more details] and in case of success
+ * creates and returns cynara structure.
+ *
+ * \par Sync (or) Async:
+ * This is a Synchronous API.
+ *
+ * \par Thread-safeness:
+ * This function is NOT thread-safe. If functions from described API are called by multithreaded
+ * application from different threads, they must be put into mutex protected critical section.
+ *
+ * \par Important notes:
+ * Structure cynara created by cynara_initialize call should be released with cynara_finish.
+ *
+ * \param[out] pp_cynara Place holder for created cynara structure.
+ * \param[in] p_conf Configuration for cynara-client library. NULL for default parameters.
+ * [TODO define and describe functions for custom parameters].
+ *
+ * \return CYNARA_API_SUCCESS on success, or error code on error.
+ */
+int cynara_initialize (cynara **pp_cynara, const cynara_configuration *p_conf);
+
+/**
+ * \par Description:
+ * Release cynara-client library and destroy structure created with cynara_initialize.
+ *
+ * \par Purpose:
+ * This API should be used to clean up after usage of cynara-client library.
+ *
+ * \par Typical use case:
+ * Once after last call to cynara_check.
+ *
+ * \par Method of function operation:
+ * This API initializes releases inner library structures [TODO describe more details]
+ * and destroys cynara structure.
+ *
+ * \par Sync (or) Async:
+ * This is a Synchronous API.
+ *
+ * \par Thread-safeness:
+ * This function is NOT thread-safe. If functions from described API are called by multithreaded
+ * application from different threads, they must be put into mutex protected critical section.
+ *
+ * \par Important notes:
+ * No other call to libcynara-client should be made after call to cynara_finish.
+ *
+ * \param[in] p_cynara Cynara structure.
+ *
+ * \return CYNARA_API_SUCCESS on success, or error code on error.
+ */
+int cynara_finish(cynara *p_cynara);
+
+/**
+ * \par Description:
+ * Check client, user access for given privilege.
+ *
+ * \par Purpose:
+ * This API should be used to check if a user running application identified as client
+ * has access to a privilege.
+ *
+ * \par Typical use case:
+ * A service want to ask trusted process (Cynara), if a client demanding access to some privilege
+ * has proper rights.
+ *
+ * \par Method of function operation:
+ * Client (a process / application) demanding access to a privilege is running as some user.
+ * For such triple an access to a privilege is checked by calling cynara.
+ * Depending on defined policy, an external application may be launched to ask user a question,
+ * e.g. if [s]he wants to allow client to use a privilege. Additional parameter client_session
+ * may be used to distinguish between client session (e.g. for allowing access only for this
+ * particular application launch).
+ *
+ * \par Sync (or) Async:
+ * This is a Synchronous API.
+ *
+ * \par Thread-safeness:
+ * This function is NOT thread-safe. If functions from described API are called by multithreaded
+ * application from different threads, they must be put into mutex protected critical section.
+ *
+ * \par Important notes:
+ * An external application may be launched to allow user interaction in granting or denying access.
+ * Call to cynara_check needs cynara structure to be created first with call to cynara_initialize.
+ *
+ * \param[in] p_cynara Cynara structure.
+ * \param[in] client Application or process identifier.
+ * \param[in] client_session Session of client (connection, launch).
+ * \param[in] user User running client.
+ * \param[in] privilege Privilege that is a subject of a check..
+ *
+ * \return CYNARA_API_SUCCESS on success (access granted), CYNARA_API_ACCESS_DENIED on access denial
+ * or other error code on error.
+ */
+int cynara_check(cynara *p_cynara, const char *client, const char *client_session, const char *user,
+        const char *privilege);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CYNARA_CLIENT_H */

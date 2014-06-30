3ad19618f81e22b6125baadaa381fa07d712b651
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2014-06-30 09:58:18 +0200

    Prepare client socket class for connecting cynara service
    Class is based on implementation imported from security-server
    in previous commit.
    
    Change-Id: I77643de6ec7321d3f6af0b395e1b1e4bf06b3774

---
+++ src/client/sockets/Socket.cpp
@@ -0,0 +1,231 @@
+/*
+ *  Copyright (c) 2000 - 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        Socket.cpp
+ * @author      Bartlomiej Grzelewski <b.grzelewski@samsung.com>
+ * @author      Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
+ * @version     1.0
+ * @brief       This file contains implementation of UNIX client socket class
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <exceptions/InitException.h>
+#include <exceptions/ServerConnectionErrorException.h>
+#include <exceptions/UnexpectedErrorException.h>
+#include <log/log.h>
+
+#include "Socket.h"
+
+namespace Cynara {
+
+Socket::Socket(const std::string &socketPath, int timeoutMiliseconds) : m_sock(-1),
+    m_socketPath(socketPath), m_pollTimeout(timeoutMiliseconds) {
+}
+
+Socket::~Socket() {
+    close();
+}
+
+void Socket::close(void) noexcept {
+    if (m_sock > -1)
+        ::close(m_sock);
+    m_sock = -1;
+}
+
+bool Socket::waitForSocket(int event) {
+    int ret;
+    pollfd desc[1];
+    desc[0].fd = m_sock;
+    desc[0].events = event;
+
+    ret = TEMP_FAILURE_RETRY(poll(desc, 1, m_pollTimeout));
+
+    if (ret == -1) {
+        int err = errno;
+        LOGE("'poll' function error [%d] : <%s>", err, strerror(err));
+        close();
+        throw UnexpectedErrorException(err, strerror(err));
+    } else if (ret == 0) {
+        LOGD("Poll timeout");
+    }
+
+    return (ret == 1);
+}
+
+int Socket::getSocketError(void) {
+    int err = 0;
+    socklen_t len = sizeof(err);
+    int ret = getsockopt(m_sock, SOL_SOCKET, SO_ERROR, &err, &len);
+    if (ret < 0) {
+        int err = errno;
+        close();
+        LOGE("'getsockopt' function error [%d] : <%s>", err, strerror(err));
+        throw UnexpectedErrorException(err, strerror(err));
+    }
+    return err;
+}
+
+bool Socket::isConnected(void) {
+    if (m_sock < 0)
+        return false;
+
+    if (getSocketError() != 0) {
+        close();
+        return false;
+    }
+
+    return true;
+}
+
+bool Socket::connect(void) {
+    sockaddr_un clientAddr;
+    int flags;
+
+    if (isConnected())
+        return true;
+
+    close();
+
+    m_sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (m_sock < 0) {
+        int err = errno;
+        LOGE("'socket' function error [%d] : <%s>", err, strerror(err));
+        throw UnexpectedErrorException(err, strerror(err));
+    }
+
+    if ((flags = fcntl(m_sock, F_GETFL, 0)) < 0 ||
+        fcntl(m_sock, F_SETFL, flags | O_NONBLOCK) < 0)
+    {
+        int err = errno;
+        close();
+        LOGE("'fcntl' function error [%d] : <%s>", err, strerror(err));
+        throw UnexpectedErrorException(err, strerror(err));
+    }
+
+    memset(&clientAddr, 0, sizeof(clientAddr));
+
+    clientAddr.sun_family = AF_UNIX;
+
+    if (m_socketPath.length() >= sizeof(clientAddr.sun_path)) {
+        close();
+        LOGE("Error: socket path <%s> is too long [%zu]. Max len is [%u]", m_socketPath.c_str(),
+             m_socketPath.length(), sizeof(clientAddr.sun_path));
+        throw InitException();
+    }
+
+    strcpy(clientAddr.sun_path, m_socketPath.c_str());
+
+    LOGD("ClientAddr.sun_path <%s>", clientAddr.sun_path);
+
+    int retval = TEMP_FAILURE_RETRY(::connect(m_sock, (struct sockaddr*)&clientAddr,
+                                            SUN_LEN(&clientAddr)));
+    int err = 0;
+    if (retval == -1) {
+        err = errno;
+        if (err == EINPROGRESS) {
+            if (!waitForSocket(POLLOUT)) {
+                return false;
+            }
+            err = getSocketError();
+        }
+        if (err == ECONNREFUSED) {
+            //no one is listening
+            return false;
+        }
+        close();
+        LOGE("'connect' function error [%d] : <%s>", err, strerror(err));
+        throw UnexpectedErrorException(err, strerror(err));
+    }
+
+    return isConnected();
+}
+
+bool Socket::sendToServer(BinaryQueue &queue) {
+    ssize_t done = 0;
+    bool retry = false;
+
+    RawBuffer buffer(queue.size());
+    queue.flattenConsume(buffer.data(), queue.size());
+
+    do {
+        if (!connect()) {
+            LOGE("Error connecting to socket");
+            throw ServerConnectionErrorException();
+        }
+
+        retry = false;
+        done = 0;
+        while ((buffer.size() - done) > 0) {
+            if (! waitForSocket(POLLOUT)) {
+                LOGE("Error in poll(POLLOUT)");
+                throw ServerConnectionErrorException();
+            }
+            ssize_t t = TEMP_FAILURE_RETRY(write(m_sock, buffer.data() + done,
+                                           buffer.size() - done));
+            if (t == -1) {
+                int err = errno;
+                if (err == EPIPE) {
+                    close();
+                    LOGN("Connection closed by server. Retrying to connect.");
+                    retry = true;
+                    break;
+                }
+                close();
+                LOGE("'write' function error [%d] : <%s>", err, strerror(err));
+                throw UnexpectedErrorException(err, strerror(err));
+            }
+            done += t;
+        }
+    } while (retry);
+
+    return true;
+}
+
+bool Socket::receiveFromServer(BinaryQueue &queue)
+{
+    if (!waitForSocket(POLLIN)) {
+        LOGE("Error in poll(POLLIN)");
+        throw ServerConnectionErrorException();
+    }
+
+    RawBuffer readBuffer(BUFSIZ);
+    ssize_t size = TEMP_FAILURE_RETRY(read(m_sock, readBuffer.data(), BUFSIZ));
+
+    if (size == -1) {
+        int err = errno;
+        LOGE("'read' function error [%d] : <%s>", err, strerror(err));
+        throw UnexpectedErrorException(err, strerror(err));
+    }
+
+    if (size == 0) {
+        LOGW("read return 0 / Connection closed by server.");
+        return false;
+    }
+    queue.appendCopy(readBuffer.data(), size);
+
+    return true;
+}
+
+} // namespace Cynara
---
+++ src/client/sockets/Socket.h
@@ -0,0 +1,79 @@
+/*
+ *  Copyright (c) 2000 - 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        Socket.h
+ * @author      Bartlomiej Grzelewski <b.grzelewski@samsung.com>
+ * @author      Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
+ * @version     1.0
+ * @brief       This file contains definition of UNIX client socket class
+ */
+
+#ifndef SRC_CLIENT_SOCKETS_SOCKET_H_
+#define SRC_CLIENT_SOCKETS_SOCKET_H_
+
+#include <string>
+
+#include <common.h>
+
+namespace Cynara {
+
+class Socket {
+private:
+    int m_sock;
+
+    std::string m_socketPath;
+    int m_pollTimeout;
+
+    void close(void) noexcept;
+
+    //returns true      if socket is ready
+    //returns false     in case of timeout
+    //throws            in critical situations
+    bool waitForSocket(int event);
+
+    //returns int       errorcode read from socket
+    //throws            in critical situations
+    int getSocketError(void);
+
+public:
+    Socket(const std::string &socketPath, int timeoutMiliseconds = -1);
+    ~Socket();
+
+    //returns bool      connection state (true - connected, false - not connected))
+    //throws            in critical situations
+    bool isConnected(void);
+
+    //returns true      if connection succeeded
+    //returns false     if connection was timeout or no one is listening
+    //throws            in critical situations
+    bool connect(void);
+
+    //returns true                              if data was successfully send to server
+    //returns false                             if connection was lost
+    //throws ServerConnectionErrorException     if cannot connect server (or timeout)
+    //throws other exceptions                   in critical situations
+    bool sendToServer(BinaryQueue &queue);
+
+    //returns true                              if data was successfully read from server
+    //returns false                             if connection was lost
+    //throws ServerConnectionErrorException     if cannot connect server (or timeout)
+    //throws other exceptions                   in critical situations
+    bool receiveFromServer(BinaryQueue &queue);
+};
+
+} // namespace Cynara
+
+#endif /* SRC_CLIENT_SOCKETS_SOCKET_H_ */

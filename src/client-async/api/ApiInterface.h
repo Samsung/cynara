tizenorg/platform.core.security.cynara

Add asynchronous client api
Change-Id: I76f4374a96a09f53dd4154a8049c42683fb39bf7

src/client-async/api/ApiInterface.h
@@ -1,0 +1,45 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        ApiInterface.h
+ * @author      Marcin Niesluchowski <m.niesluchow@samsung.com>
+ * @version     1.0
+ * @brief       This file contains libcynara-client-async API interface definition.
+ */
+
+#ifndef SRC_CLIENT_ASYNC_API_APIINTERFACE_H_
+#define SRC_CLIENT_ASYNC_API_APIINTERFACE_H_
+
+#include <string>
+
+namespace Cynara {
+
+class ApiInterface {
+public:
+    ApiInterface() = default;
+    virtual ~ApiInterface() {};
+
+    virtual int connect(int &sockFd) = 0;
+    virtual int check(const std::string &client, const std::string &session,
+                      const std::string &user, const std::string &privilege,
+                      uint16_t &checkId) = 0;
+    virtual int receive(uint16_t &checkId) = 0;
+    virtual int cancel(const uint16_t checkId) = 0;
+};
+
+} // namespace Cynara
+
+#endif /* SRC_CLIENT_ASYNC_API_APIINTERFACE_H_ */

src/client-async/logic/Logic.cpp
@@ -1,0 +1,54 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        Logic.cpp
+ * @author      Marcin Niesluchowski <m.niesluchow@samsung.com>
+ * @version     1.0
+ * @brief       This file contains implementation of Logic class - main
+ *              libcynara-client-async class
+ */
+
+#include <common.h>
+
+#include <cynara-client-async.h>
+
+#include "Logic.h"
+
+namespace Cynara {
+
+int Logic::connect(int &sockFd UNUSED) noexcept
+{
+    return CYNARA_ASYNC_API_SUCCESS;
+}
+
+int Logic::check(const std::string &client UNUSED, const std::string &session UNUSED,
+                 const std::string &user UNUSED, const std::string &privilege UNUSED,
+                 uint16_t &checkId UNUSED) noexcept
+{
+    return CYNARA_ASYNC_API_SUCCESS;
+}
+
+int Logic::receive(uint16_t &checkId UNUSED) noexcept
+{
+    return CYNARA_ASYNC_API_SUCCESS;
+}
+
+int Logic::cancel(const uint16_t checkId UNUSED) noexcept
+{
+    return CYNARA_ASYNC_API_SUCCESS;
+}
+
+} // namespace Cynara

src/client-async/logic/Logic.h
@@ -1,0 +1,46 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        Logic.h
+ * @author      Marcin Niesluchowski <m.niesluchow@samsung.com>
+ * @version     1.0
+ * @brief       This file contains declaration of Logic class - main
+ *              libcynara-client-async class
+ */
+
+#ifndef SRC_CLIENT_ASYNC_LOGIC_LOGIC_H_
+#define SRC_CLIENT_ASYNC_LOGIC_LOGIC_H_
+
+#include <api/ApiInterface.h>
+
+namespace Cynara {
+
+class Logic : public ApiInterface {
+public:
+    Logic() = default;
+    virtual ~Logic() {};
+
+    virtual int connect(int &sockFd) noexcept;
+    virtual int check(const std::string &client, const std::string &session,
+                      const std::string &user, const std::string &privilege,
+                      uint16_t &checkId) noexcept;
+    virtual int receive(uint16_t &checkId) noexcept;
+    virtual int cancel(const uint16_t checkId) noexcept;
+};
+
+} // namespace Cynara
+
+#endif /* SRC_CLIENT_ASYNC_LOGIC_LOGIC_H_ */

Samsung/cynara

src/client-async/api/ApiInterface.h
@@ -1,0 +1,47 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        ApiInterface.h
+ * @author      Marcin Niesluchowski <m.niesluchow@samsung.com>
+ * @version     1.0
+ * @brief       This file contains libcynara-client-async API interface definition.
+ */
+
+#ifndef SRC_CLIENT_ASYNC_API_APIINTERFACE_H_
+#define SRC_CLIENT_ASYNC_API_APIINTERFACE_H_
+
+#include <string>
+
+#include <cynara-client-async.h>
+
+namespace Cynara {
+
+class ApiInterface {
+public:
+    ApiInterface() = default;
+    virtual ~ApiInterface() {};
+
+    virtual int connect(int &sockFd) = 0;
+    virtual int check(const std::string &client, const std::string &session,
+                      const std::string &user, const std::string &privilege,
+                      cynara_check_id &checkId) = 0;
+    virtual int receive(cynara_check_id &checkId) = 0;
+    virtual int cancel(const cynara_check_id checkId) = 0;
+};
+
+} // namespace Cynara
+
+#endif /* SRC_CLIENT_ASYNC_API_APIINTERFACE_H_ */

src/client-async/logic/Logic.cpp
@@ -1,0 +1,52 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        Logic.cpp
+ * @author      Marcin Niesluchowski <m.niesluchow@samsung.com>
+ * @version     1.0
+ * @brief       This file contains implementation of Logic class - main
+ *              libcynara-client-async class
+ */
+
+#include <common.h>
+
+#include "Logic.h"
+
+namespace Cynara {
+
+int Logic::connect(int &sockFd UNUSED) noexcept
+{
+    return CYNARA_ASYNC_API_SUCCESS;
+}
+
+int Logic::check(const std::string &client UNUSED, const std::string &session UNUSED,
+                 const std::string &user UNUSED, const std::string &privilege UNUSED,
+                 cynara_check_id &checkId UNUSED) noexcept
+{
+    return CYNARA_ASYNC_API_SUCCESS;
+}
+
+int Logic::receive(cynara_check_id &checkId UNUSED) noexcept
+{
+    return CYNARA_ASYNC_API_SUCCESS;
+}
+
+int Logic::cancel(const cynara_check_id checkId UNUSED) noexcept
+{
+    return CYNARA_ASYNC_API_SUCCESS;
+}
+
+} // namespace Cynara

src/client-async/logic/Logic.h
@@ -1,0 +1,47 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        Logic.h
+ * @author      Marcin Niesluchowski <m.niesluchow@samsung.com>
+ * @version     1.0
+ * @brief       This file contains declaration of Logic class - main
+ *              libcynara-client-async class
+ */
+
+#ifndef SRC_CLIENT_ASYNC_LOGIC_LOGIC_H_
+#define SRC_CLIENT_ASYNC_LOGIC_LOGIC_H_
+
+#include <api/ApiInterface.h>
+#include <cynara-client-async.h>
+
+namespace Cynara {
+
+class Logic : public ApiInterface {
+public:
+    Logic() = default;
+    virtual ~Logic() {};
+
+    virtual int connect(int &sockFd) noexcept;
+    virtual int check(const std::string &client, const std::string &session,
+                      const std::string &user, const std::string &privilege,
+                      cynara_check_id &checkId) noexcept;
+    virtual int receive(cynara_check_id &checkId) noexcept;
+    virtual int cancel(const cynara_check_id checkId) noexcept;
+};
+
+} // namespace Cynara
+
+#endif /* SRC_CLIENT_ASYNC_LOGIC_LOGIC_H_ */
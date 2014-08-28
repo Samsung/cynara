tizenorg/platform.core.security.cynara

Add ClientSession type
Change-Id: Ia62dac02a652c2f252708ed05320eb66ea5506b1

src/common/types/ClientSession.h
@@ -1,0 +1,34 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/*
+ * @file        Session.h
+ * @author      Zofia Abramowska <z.abramowska@samsung.com>
+ * @version     1.0
+ * @brief       Description of user defined session type
+ */
+
+#ifndef CYNARA_COMMON_TYPES_CLIENTSESSION_H
+#define CYNARA_COMMON_TYPES_CLIENTSESSION_H
+
+#include <string>
+
+namespace Cynara {
+
+typedef std::string ClientSession;
+
+} // namespace Cynara
+
+#endif /* CYNARA_COMMON_TYPES_CLIENTSESSION_H */

Samsung/cynara

src/common/types/ClientSession.h
@@ -1,0 +1,34 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/*
+ * @file        ClientSession.h
+ * @author      Zofia Abramowska <z.abramowska@samsung.com>
+ * @version     1.0
+ * @brief       Description of user defined session type
+ */
+
+#ifndef SRC_COMMON_TYPES_CLIENTSESSION_H_
+#define SRC_COMMON_TYPES_CLIENTSESSION_H_
+
+#include <string>
+
+namespace Cynara {
+
+typedef std::string ClientSession;
+
+} // namespace Cynara
+
+#endif /* SRC_COMMON_TYPES_CLIENTSESSION_H_ */
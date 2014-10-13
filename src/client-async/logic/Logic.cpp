tizenorg/platform.core.security.cynara

Add creating, sending and receiving check requests
cynara_async_create_request() and cynara_async_process() functions work
according to the API header excluding possibility to cancel request.

Change-Id: Ide56933f1cec14a7144df6098ef8f7b5d8e1d25c

src/client-async/logic/Logic.cpp
@@ -30,6 +30,8 @@
 #include <log/log.h>
 #include <plugins/NaiveInterpreter.h>
 #include <protocol/ProtocolClient.h>
+#include <request/CheckRequest.h>
+#include <response/CheckResponse.h>
 #include <sockets/Socket.h>
 #include <sockets/SocketPath.h>
 
@@ -50,7 +52,9 @@ Logic::Logic(cynara_status_callback callback, void *userStatusData)
 }
 
 Logic::~Logic() {
-    onDisconnected();
+    for (auto &kv : m_checks)
+        kv.second.callback().onFinish(kv.first);
+    m_statusCallback.onDisconnected();
 }
 
 int Logic::checkCache(const std::string &client, const std::string &session,
@@ -67,16 +71,32 @@ int Logic::checkCache(const std::string &client, const std::string &session,
     return ret;
 }
 
-int Logic::createRequest(const std::string &client UNUSED, const std::string &session UNUSED,
-                         const std::string &user UNUSED, const std::string &privilege UNUSED,
-                         cynara_check_id &checkId UNUSED, cynara_response_callback callback UNUSED,
-                         void *userResponseData UNUSED) noexcept {
-    int ret = connect();
+int Logic::createRequest(const std::string &client, const std::string &session,
+                         const std::string &user, const std::string &privilege,
+                         cynara_check_id &checkId, cynara_response_callback callback,
+                         void *userResponseData) noexcept {
+    int ret = ensureConnection();
     if (ret != CYNARA_API_SUCCESS)
         return ret;
 
-    // MOCKUP
-    return CYNARA_API_MAX_PENDING_REQUESTS;
+    ProtocolFrameSequenceNumber sequenceNumber;
+    try {
+        if (!m_sequenceContainer.generate(sequenceNumber))
+            return CYNARA_API_MAX_PENDING_REQUESTS;
+
+        PolicyKey key(client, user, privilege);
+        ResponseCallback responseCallback(callback, userResponseData);
+        m_checks.insert(CheckPair(sequenceNumber, CheckData(key, session, responseCallback)));
+        m_socket->appendRequest(std::make_shared<CheckRequest>(key, sequenceNumber));
+    } catch (const std::bad_alloc &) {
+        return CYNARA_API_OUT_OF_MEMORY;
+    }
+
+    m_statusCallback.onStatusChange(m_socket->getSockFd(),
+                                    cynara_async_status::CYNARA_STATUS_FOR_RW);
+    checkId = static_cast<cynara_check_id>(sequenceNumber);
+
+    return CYNARA_API_SUCCESS;
 }
 
 int Logic::process(void) noexcept {
@@ -86,12 +106,28 @@ int Logic::process(void) noexcept {
         return ret;
     }
 
-    // MOCKUP
-    return CYNARA_API_SUCCESS;
+    while(true) {
+        ret = processOut();
+        if (ret == CYNARA_API_SUCCESS) {
+            ret = processIn();
+            if (ret == CYNARA_API_SUCCESS)
+                return ret;
+        }
+        if (ret == CYNARA_API_SERVICE_NOT_AVAILABLE) {
+            onDisconnected();
+            ret = connect(connectionInProgress);
+            if (ret == CYNARA_API_SUCCESS) {
+                if (connectionInProgress)
+                    return CYNARA_API_SUCCESS;
+                continue;
+            }
+        }
+        return ret;
+    }
 }
 
 int Logic::cancelRequest(cynara_check_id checkId UNUSED) noexcept {
-    int ret = connect();
+    int ret = ensureConnection();
     if (ret != CYNARA_API_SUCCESS)
         return ret;
 
@@ -111,8 +147,104 @@ int Logic::checkCacheValid(void) {
     }
 }
 
-void Logic::prepareRequestsToSend(void) {
+int Logic::prepareRequestsToSend(void) {
+    try {
+        for (auto &kv : m_checks) {
+            // MOCKUP
+            m_socket->appendRequest(std::make_shared<CheckRequest>(kv.second.key(), kv.first));
+        }
+    } catch (const std::bad_alloc &) {
+        return CYNARA_API_OUT_OF_MEMORY;
+    }
+    m_statusCallback.onStatusChange(m_socket->getSockFd(),
+                                    socketDataStatus());
+    return CYNARA_API_SUCCESS;
+}
+
+int Logic::processOut(void) {
+    Socket::Socket::SendStatus status;
+    try {
+        status = m_socket->sendToCynara();
+    } catch (const UnexpectedErrorException &ex) {
+        LOGE(ex.message().c_str());
+        return CYNARA_API_UNKNOWN_ERROR;
+    } catch (std::bad_alloc &) {
+        return CYNARA_API_OUT_OF_MEMORY;
+    }
+    switch (status) {
+        case Socket::SendStatus::ALL_DATA_SENT:
+            m_statusCallback.onStatusChange(m_socket->getSockFd(),
+                                            cynara_async_status::CYNARA_STATUS_FOR_READ);
+        case Socket::SendStatus::PARTIAL_DATA_SENT:
+            return CYNARA_API_SUCCESS;
+        default:
+            return CYNARA_API_SERVICE_NOT_AVAILABLE;
+    }
+}
+
+int Logic::processCheckResponse(CheckResponsePtr checkResponse) {
+    LOGD("checkResponse: policyType = [%" PRIu16 "], metadata = <%s>",
+         checkResponse->m_resultRef.policyType(),
+         checkResponse->m_resultRef.metadata().c_str());
+
+    auto it = m_checks.find(checkResponse->sequenceNumber());
+    if (it == m_checks.end()) {
+        LOGC("Critical error. Unknown response received: sequenceNumber = [%" PRIu16 "]",
+             checkResponse->sequenceNumber());
+        return CYNARA_API_UNKNOWN_ERROR;
+    }
+    int result;
+    try {
+        result = m_cache->update(it->second.session(), it->second.key(), checkResponse->m_resultRef);
+    } catch (const std::bad_alloc &) {
+        return CYNARA_API_OUT_OF_MEMORY;
+    }
     // MOCKUP
+    it->second.callback().onAnswer(static_cast<cynara_check_id>(it->first), result);
+    m_checks.erase(it);
+    return CYNARA_API_SUCCESS;
+}
+
+int Logic::processResponses(void) {
+    ResponsePtr response;
+    CheckResponsePtr checkResponse;
+    while (true) {
+        try {
+            response = m_socket->getResponse();
+            if (!response)
+                break;
+        } catch (const std::bad_alloc &) {
+            return CYNARA_API_OUT_OF_MEMORY;
+        }
+
+        checkResponse = std::dynamic_pointer_cast<CheckResponse>(response);
+        if (checkResponse) {
+            int ret = processCheckResponse(checkResponse);
+            if (ret != CYNARA_API_SUCCESS)
+                return ret;
+            continue;
+        }
+        // MOCKUP
+        LOGC("Critical error. Casting Response to CheckResponse failed.");
+        return CYNARA_API_UNKNOWN_ERROR;
+    }
+    return CYNARA_API_SUCCESS;
+}
+
+int Logic::processIn(void) {
+    bool connected;
+    try {
+        connected = m_socket->receiveFromCynara();
+    } catch (const UnexpectedErrorException &ex) {
+        LOGE(ex.message().c_str());
+        return CYNARA_API_UNKNOWN_ERROR;
+    } catch (const std::bad_alloc &) {
+        return CYNARA_API_OUT_OF_MEMORY;
+    }
+    if (!connected) {
+        return CYNARA_API_SERVICE_NOT_AVAILABLE;
+    }
+    return processResponses();
 }
 
 cynara_async_status Logic::socketDataStatus(void) {
@@ -121,7 +253,7 @@ cynara_async_status Logic::socketDataStatus(void) {
         : cynara_async_status::CYNARA_STATUS_FOR_READ;
 }
 
-int Logic::connect(void) {
+int Logic::ensureConnection(void) {
     try {
         if (m_socket->isConnected())
             return CYNARA_API_SUCCESS;
@@ -131,6 +263,11 @@ int Logic::connect(void) {
     }
     onDisconnected();
 
+    bool connectionInProgress;
+    return connect(connectionInProgress);
+}
+
+int Logic::connect(bool &connectionInProgress) {
     Socket::ConnectionStatus status;
     try {
         status = m_socket->connect();
@@ -138,18 +275,26 @@ int Logic::connect(void) {
         LOGE(ex.message().c_str());
         return CYNARA_API_UNKNOWN_ERROR;
     }
+    int ret;
     switch (status) {
         case Socket::ConnectionStatus::CONNECTION_SUCCEEDED:
-            prepareRequestsToSend();
+            ret = prepareRequestsToSend();
+            if (ret != CYNARA_API_SUCCESS)
+                return ret;
             m_statusCallback.onStatusChange(m_socket->getSockFd(),
                                             socketDataStatus());
+            connectionInProgress = false;
             return CYNARA_API_SUCCESS;
         case Socket::ConnectionStatus::CONNECTION_IN_PROGRESS:
-            prepareRequestsToSend();
+            ret = prepareRequestsToSend();
+            if (ret != CYNARA_API_SUCCESS)
+                return ret;
             m_statusCallback.onStatusChange(m_socket->getSockFd(),
                                             cynara_async_status::CYNARA_STATUS_FOR_RW);
+            connectionInProgress = true;
             return CYNARA_API_SUCCESS;
         default:
+            onServiceNotAvailable();
             return CYNARA_API_SERVICE_NOT_AVAILABLE;
     }
 }
@@ -176,11 +321,18 @@ int Logic::completeConnection(bool &connectionInProgress) {
             return CYNARA_API_SUCCESS;
         default:
             onDisconnected();
+            onServiceNotAvailable();
             return CYNARA_API_SERVICE_NOT_AVAILABLE;
     }
 }
 
-void Logic::onDisconnected(void) {
+void Logic::onServiceNotAvailable()
+{
+    for (auto &kv : m_checks)
+        kv.second.callback().onDisconnected(kv.first);
+}
+
+void Logic::onDisconnected() {
     m_cache->clear();
     m_statusCallback.onDisconnected();
 }

src/client-async/sequence/SequenceContainer.h
@@ -1,0 +1,48 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/**
+ * @file        src/client-async/sequence/SequenceContainer.h
+ * @author      Marcin Niesluchowski <m.niesluchow@samsung.com>
+ * @version     1.0
+ * @brief       This file contains declaration of SequenceContainer class for
+ *              storing check requests identifiers in libcynara-client-async
+ */
+
+#ifndef SRC_CLIENT_ASYNC_SEQUENCE_SEQUENCECONTAINER_H_
+#define SRC_CLIENT_ASYNC_SEQUENCE_SEQUENCECONTAINER_H_
+
+#include <vector>
+
+#include <types/ProtocolFields.h>
+
+namespace Cynara {
+
+class SequenceContainer {
+public:
+    SequenceContainer();
+    ~SequenceContainer() {}
+
+    bool generate(ProtocolFrameSequenceNumber &sequenceNumber);
+    bool remove(ProtocolFrameSequenceNumber sequenceNumber);
+    void clear(void);
+
+private:
+    std::vector<int> m_sequenceVector;
+};
+
+} // namespace Cynara
+
+#endif // SRC_CLIENT_ASYNC_SEQUENCE_SEQUENCECONTAINER_H_

Samsung/cynara

Change-Id: I9818be674d58da5bd431a08b7faf47dfe0157289

src/client-async/logic/Logic.cpp
@@ -26,10 +26,13 @@
 #include <cache/CapacityCache.h>
 #include <common.h>
 #include <exceptions/Exception.h>
+#include <exceptions/NoMemoryException.h>
 #include <exceptions/UnexpectedErrorException.h>
 #include <log/log.h>
 #include <plugins/NaiveInterpreter.h>
 #include <protocol/ProtocolClient.h>
+#include <request/CheckRequest.h>
+#include <response/CheckResponse.h>
 #include <sockets/Socket.h>
 #include <sockets/SocketPath.h>
 
@@ -49,7 +52,9 @@ Logic::Logic(cynara_status_callback callback, void *userStatusData)
 }
 
 Logic::~Logic() {
-    onDisconnected();
+    for (auto &kv : m_checks)
+        kv.second.callback().onFinish(kv.first);
+    m_statusCallback.onDisconnected();
 }
 
 int Logic::checkCache(const std::string &client, const std::string &session,
@@ -60,25 +65,41 @@ int Logic::checkCache(const std::string &client, const std::string &session,
     return m_cache->get(session, PolicyKey(client, user, privilege));
 }
 
-int Logic::createRequest(const std::string &client UNUSED, const std::string &session UNUSED,
-                         const std::string &user UNUSED, const std::string &privilege UNUSED,
-                         cynara_check_id &checkId UNUSED, cynara_response_callback callback UNUSED,
-                         void *userResponseData UNUSED) {
+int Logic::createRequest(const std::string &client, const std::string &session,
+                         const std::string &user, const std::string &privilege,
+                         cynara_check_id &checkId, cynara_response_callback callback,
+                         void *userResponseData) {
     if (!ensureConnection())
         return CYNARA_API_SERVICE_NOT_AVAILABLE;
 
-    // MOCKUP
-    return CYNARA_API_MAX_PENDING_REQUESTS;
+    ProtocolFrameSequenceNumber sequenceNumber;
+    if (!m_sequenceContainer.get(sequenceNumber))
+        return CYNARA_API_MAX_PENDING_REQUESTS;
+
+    PolicyKey key(client, user, privilege);
+    ResponseCallback responseCallback(callback, userResponseData);
+    m_checks.insert(CheckPair(sequenceNumber, CheckData(key, session, responseCallback)));
+    m_socketClient->appendRequest(std::make_shared<CheckRequest>(key, sequenceNumber));
+
+    m_statusCallback.onStatusChange(m_socketClient->getSockFd(),
+                                    cynara_async_status::CYNARA_STATUS_FOR_RW);
+    checkId = static_cast<cynara_check_id>(sequenceNumber);
+
+    return CYNARA_API_SUCCESS;
 }
 
 int Logic::process(void) {
     bool completed;
-    int ret = completeConnection(completed);
-    if (!completed)
-        return ret;
-
-    // MOCKUP
-    return CYNARA_API_SUCCESS;
+    while (true) {
+        int ret = completeConnection(completed);
+        if (!completed)
+            return ret;
+        if (processOut() && processIn())
+            return CYNARA_API_SUCCESS;
+        onDisconnected();
+        if (!connect())
+            return CYNARA_API_SERVICE_NOT_AVAILABLE;
+    }
 }
 
 int Logic::cancelRequest(cynara_check_id checkId UNUSED) {
@@ -94,7 +115,67 @@ bool Logic::checkCacheValid(void) {
 }
 
 void Logic::prepareRequestsToSend(void) {
+    for (auto &kv : m_checks) {
+        // MOCKUP
+        m_socketClient->appendRequest(std::make_shared<CheckRequest>(kv.second.key(), kv.first));
+    }
+}
+
+bool Logic::processOut(void) {
+    switch (m_socketClient->sendToCynara()) {
+        case Socket::SendStatus::ALL_DATA_SENT:
+            m_statusCallback.onStatusChange(m_socketClient->getSockFd(),
+                                            cynara_async_status::CYNARA_STATUS_FOR_READ);
+        case Socket::SendStatus::PARTIAL_DATA_SENT:
+            return true;
+        default:
+            return false;
+    }
+}
+
+void Logic::processCheckResponse(CheckResponsePtr checkResponse) {
+    LOGD("checkResponse: policyType = [%" PRIu16 "], metadata = <%s>",
+         checkResponse->m_resultRef.policyType(),
+         checkResponse->m_resultRef.metadata().c_str());
+
+    auto it = m_checks.find(checkResponse->sequenceNumber());
+    if (it == m_checks.end()) {
+        LOGC("Critical error. Unknown response received: sequenceNumber = [%" PRIu16 "]",
+             checkResponse->sequenceNumber());
+        throw UnexpectedErrorException("Unexpected response from cynara service");
+    }
+    int result = m_cache->update(it->second.session(), it->second.key(),
+                                 checkResponse->m_resultRef);
     // MOCKUP
+    it->second.callback().onAnswer(static_cast<cynara_check_id>(it->first), result);
+    m_sequenceContainer.release(it->first);
+    m_checks.erase(it);
+}
+
+void Logic::processResponses(void) {
+    ResponsePtr response;
+    CheckResponsePtr checkResponse;
+    while (true) {
+        response = m_socketClient->getResponse();
+        if (!response)
+            break;
+
+        checkResponse = std::dynamic_pointer_cast<CheckResponse>(response);
+        if (checkResponse) {
+            processCheckResponse(checkResponse);
+            continue;
+        }
+        // MOCKUP
+        LOGC("Critical error. Casting Response to CheckResponse failed.");
+        throw UnexpectedErrorException("Unexpected response from cynara service");
+    }
+}
+
+bool Logic::processIn(void) {
+    if (!m_socketClient->receiveFromCynara())
+        return false;
+    processResponses();
+    return true;
 }
 
 cynara_async_status Logic::socketDataStatus(void) {
@@ -106,6 +187,11 @@ bool Logic::ensureConnection(void) {
     if (m_socketClient->isConnected())
         return true;
     onDisconnected();
+
+    return connect();
+}
+
+bool Logic::connect(void) {
     switch (m_socketClient->connect()) {
         case Socket::ConnectionStatus::CONNECTION_SUCCEEDED:
             prepareRequestsToSend();
@@ -117,6 +203,7 @@ bool Logic::ensureConnection(void) {
                                             cynara_async_status::CYNARA_STATUS_FOR_RW);
             return true;
         default:
+            onServiceNotAvailable();
             return false;
     }
 }
@@ -136,8 +223,17 @@ int Logic::completeConnection(bool &completed) {
         default:
             completed = false;
             onDisconnected();
+            onServiceNotAvailable();
             return CYNARA_API_SERVICE_NOT_AVAILABLE;
     }
+}
+
+void Logic::onServiceNotAvailable(void)
+{
+    for (auto &kv : m_checks)
+        kv.second.callback().onDisconnected(kv.first);
+    m_checks.clear();
+    m_sequenceContainer.clear();
 }
 
 void Logic::onDisconnected(void) {

src/client-async/sequence/SequenceContainer.h
@@ -1,0 +1,48 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/**
+ * @file        src/client-async/sequence/SequenceContainer.h
+ * @author      Marcin Niesluchowski <m.niesluchow@samsung.com>
+ * @version     1.0
+ * @brief       This file contains declaration of SequenceContainer class for
+ *              storing check requests identifiers in libcynara-client-async
+ */
+
+#ifndef SRC_CLIENT_ASYNC_SEQUENCE_SEQUENCECONTAINER_H_
+#define SRC_CLIENT_ASYNC_SEQUENCE_SEQUENCECONTAINER_H_
+
+#include <vector>
+
+#include <types/ProtocolFields.h>
+
+namespace Cynara {
+
+class SequenceContainer {
+public:
+    SequenceContainer();
+    ~SequenceContainer() {}
+
+    bool get(ProtocolFrameSequenceNumber &sequenceNumber);
+    bool release(ProtocolFrameSequenceNumber sequenceNumber);
+    void clear(void);
+
+private:
+    std::vector<int> m_sequenceVector;
+};
+
+} // namespace Cynara
+
+#endif // SRC_CLIENT_ASYNC_SEQUENCE_SEQUENCECONTAINER_H_
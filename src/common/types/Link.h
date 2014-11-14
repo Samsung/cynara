9ce6ffbb9656fd0682db12e9f94ad13465b6fe86
Adam Malinowski <a.malinowsk2@partner.samsung.com>
2014-11-14 18:58:37 +0100

    Add LinkId type
    This type will be used to identify clients and agents.
    
    Change-Id: I6f6808919a6c8694ad8c3e51d826dc223541325f

---
+++ src/common/types/Link.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/**
+ * @file        src/common/types/Link.h
+ * @author      Adam Malinowski <a.malinowsk2@partner.samsung.com>
+ * @version     1.0
+ * @brief       This file defines client connection related types
+ */
+
+#ifndef SRC_COMMON_TYPES_LINK_H_
+#define SRC_COMMON_TYPES_LINK_H_
+
+#include <containers/BinaryQueue.h>
+
+namespace Cynara {
+
+typedef BinaryQueuePtr LinkId;
+
+} // namespace Cynara
+
+#endif /* SRC_COMMON_TYPES_LINK_H_ */

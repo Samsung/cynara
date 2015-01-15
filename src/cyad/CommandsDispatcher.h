tizenorg/platform.core.security.cynara

Implement --erase option in Cyad
This option runs cynara_admin_erase() API function
to erase policies from Cynara's database.

Change-Id: I3659971657ef23fcd1c27850f56f32e57938aa6d

src/cyad/CommandsDispatcher.h
@@ -44,6 +44,7 @@ public:
     virtual int execute(SetBucketCyadCommand &);
     virtual int execute(SetPolicyCyadCommand &);
     virtual int execute(SetPolicyBulkCyadCommand &);
+    virtual int execute(EraseCyadCommand &);
 
 private:
     // TODO: Get argv[0] instead of hardcoded name
@@ -63,6 +64,15 @@ private:
                                     "  -t, --type=<policy>            policy\n"
                                     "  -m, --metadata=<metadata>      metadata for policy\n"
                                     "  -f, --bulk=<filename>          path or - for stdin\n"
+                                    "\n"
+                                    "Policy erase options (with -e or --erase)\n"
+                                    "  -e, --erase=<name>             name of bucket"
+                                                                     " to start from\n"
+                                    "  -r, --recursive=<yes|no>       if linked buckets should be"
+                                                                     " processed as well\n"
+                                    "  -c, --client=<client>          client value\n"
+                                    "  -u, --user=<user>              user value\n"
+                                    "  -r, --privilege=<privilege>    privilege value\n"
                                     "\n"
                                     "Help options:\n"
                                     "  -h, --help                     print help message";

Samsung/cynara

src/cyad/CommandsDispatcher.h
@@ -44,6 +44,7 @@ public:
     virtual int execute(SetBucketCyadCommand &);
     virtual int execute(SetPolicyCyadCommand &);
     virtual int execute(SetPolicyBulkCyadCommand &);
+    virtual int execute(EraseCyadCommand &);
 
 private:
     // TODO: Get argv[0] instead of hardcoded name
@@ -63,6 +64,15 @@ private:
                                     "  -t, --type=<policy>            policy type\n"
                                     "  -m, --metadata=<metadata>      metadata for policy\n"
                                     "  -f, --bulk=<filename>          path or - for stdin\n"
+                                    "\n"
+                                    "Policy erase options (with -e or --erase)\n"
+                                    "  -e, --erase=<name>             name of bucket"
+                                                                     " to start from\n"
+                                    "  -r, --recursive=<yes|no>       if linked buckets should be"
+                                                                     " processed as well\n"
+                                    "  -c, --client=<client>          client value\n"
+                                    "  -u, --user=<user>              user value\n"
+                                    "  -p, --privilege=<privilege>    privilege value\n"
                                     "\n"
                                     "Help options:\n"
                                     "  -h, --help                     print help message";
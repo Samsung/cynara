tizenorg/platform.core.security.cynara

Move bucket separators to PathConfig::StoragePath
This patch removes both bucket separators - for fields as well as for
records - from StorageSerializer. To this point they could be accessed
using provided static member functions. This is no longer possible, as
StorageSerializer has to be rewritten using templates.

Change-Id: Ib010bd0b125a1a93da9983d9bdd7b8f75cbbc191

src/common/config/PathConfig.h
@@ -45,8 +45,10 @@ extern const std::string lockFile;
 extern const std::string indexFilename;
 extern const std::string guardFilename;
 extern const std::string checksumFilename;
-extern const std::string bucketFilenamePrefix;
 extern const std::string backupFilenameSuffix;
+extern const std::string bucketFilenamePrefix;
+extern const char bucketFieldSeparator;
+extern const char bucketRecordSeparator;
 } // namespace StoragePath
 
 namespace PluginPath {

src/cyad/AdminPolicyParser.cpp
@@ -20,6 +20,7 @@
  * @brief       Parses policies from input stream
  */
 
+#include <config/PathConfig.h>
 #include <exceptions/BucketRecordCorruptedException.h>
 #include <storage/StorageDeserializer.h>
 #include <storage/StorageSerializer.h>
@@ -35,7 +36,7 @@ CynaraAdminPolicies parse(const std::shared_ptr<std::istream> &input) {
 
     for (std::size_t lineNum = 1; !input->eof(); ++lineNum) {
         std::string line;
-        std::getline(*input, line, StorageSerializer::recordSeparator());
+        std::getline(*input, line, PathConfig::StoragePath::bucketRecordSeparator);
 
         if (line.empty())
             break;

src/storage/BucketDeserializer.cpp
@@ -25,6 +25,7 @@
 #include <string>
 #include <vector>
 
+#include <config/PathConfig.h>
 #include <exceptions/BucketRecordCorruptedException.h>
 #include <types/PolicyCollection.h>
 #include <types/PolicyResult.h>
@@ -43,7 +44,7 @@ PolicyCollection BucketDeserializer::loadPolicies(void) {
     // TODO: Get someone smart to do error checking on stream
     for (std::size_t lineNum = 1; !m_inStream->eof(); ++lineNum) {
         std::string line;
-        std::getline(*m_inStream, line, StorageSerializer::recordSeparator());
+        std::getline(*m_inStream, line, PathConfig::StoragePath::bucketRecordSeparator);
 
         if (line.empty())
             break;
@@ -67,7 +68,7 @@ PolicyKey BucketDeserializer::parseKey(const std::string &line, std::size_t &beg
     std::array<std::string, 3> keyFeatures;
 
     for (std::size_t tokenNum = 0; tokenNum < keyFeatures.size(); ++tokenNum) {
-        auto endToken = line.find(StorageSerializer::fieldSeparator(), beginToken);
+        auto endToken = line.find(PathConfig::StoragePath::bucketFieldSeparator, beginToken);
         if (endToken != std::string::npos) {
             keyFeatures[tokenNum] = line.substr(beginToken, endToken - beginToken);
             beginToken = endToken + 1;

src/storage/StorageDeserializer.cpp
@@ -24,6 +24,7 @@
 #include <memory>
 #include <string>
 
+#include <config/PathConfig.h>
 #include <exceptions/BucketDeserializationException.h>
 #include <exceptions/BucketRecordCorruptedException.h>
 #include <types/PolicyType.h>
@@ -46,7 +47,7 @@ void StorageDeserializer::initBuckets(Buckets &buckets) {
 
     for (std::size_t lineNum = 1; !m_inStream->eof(); ++lineNum) {
         std::string line;
-        std::getline(*m_inStream, line, StorageSerializer::recordSeparator());
+        std::getline(*m_inStream, line, PathConfig::StoragePath::bucketRecordSeparator);
 
         if (line.empty())
             break;
@@ -80,7 +81,7 @@ void StorageDeserializer::loadBuckets(Buckets &buckets) {
 
 PolicyBucketId StorageDeserializer::parseBucketId(const std::string &line,
                                                     std::size_t &beginToken) {
-    auto bucketNameEndToken = line.find(StorageSerializer::fieldSeparator(), beginToken);
+    auto bucketNameEndToken = line.find(PathConfig::StoragePath::bucketFieldSeparator, beginToken);
     if (bucketNameEndToken != std::string::npos) {
         auto bucketName = line.substr(beginToken, bucketNameEndToken - beginToken);
         beginToken = bucketNameEndToken + 1;

src/storage/StorageSerializer.h
@@ -27,6 +27,7 @@
 #include <fstream>
 #include <memory>
 
+#include <config/PathConfig.h>
 #include <types/PolicyBucket.h>
 #include <types/PolicyBucketId.h>
 #include <types/PolicyCollection.h>
@@ -56,13 +57,13 @@ protected:
     inline void dumpFields(const Arg1 &arg1, const Args&... args) {
         dump(arg1);
         if (sizeof...(Args) > 0) {
-            *m_outStream << fieldSeparator();
+            *m_outStream << PathConfig::StoragePath::bucketFieldSeparator;
         }
         dumpFields(args...);
     }
 
     inline void dumpFields(void) {
-        *m_outStream << recordSeparator();
+        *m_outStream << PathConfig::StoragePath::bucketRecordSeparator;
     }
 
     void dump(const PolicyKeyFeature &keyFeature);
@@ -72,18 +73,6 @@ protected:
 
 private:
     std::shared_ptr<std::ostream> m_outStream;
-
-    static char m_fieldSeparator;
-    static char m_recordSeparator;
-
-public:
-    static const char &fieldSeparator(void) {
-        return m_fieldSeparator;
-    }
-
-    static const char &recordSeparator(void) {
-        return m_recordSeparator;
-    }
 };
 
 } /* namespace Cynara */

Samsung/cynara

src/common/config/PathConfig.h
@@ -45,8 +45,10 @@ extern const std::string lockFile;
 extern const std::string indexFilename;
 extern const std::string guardFilename;
 extern const std::string checksumFilename;
-extern const std::string bucketFilenamePrefix;
 extern const std::string backupFilenameSuffix;
+extern const std::string bucketFilenamePrefix;
+extern const char fieldSeparator;
+extern const char recordSeparator;
 } // namespace StoragePath
 
 namespace PluginPath {

src/cyad/AdminPolicyParser.cpp
@@ -20,9 +20,9 @@
  * @brief       Parses policies from input stream
  */
 
+#include <config/PathConfig.h>
 #include <exceptions/BucketRecordCorruptedException.h>
 #include <storage/StorageDeserializer.h>
-#include <storage/StorageSerializer.h>
 
 #include "AdminPolicyParser.h"
 
@@ -35,7 +35,7 @@ CynaraAdminPolicies parse(const std::shared_ptr<std::istream> &input,
     CynaraAdminPolicies policies;
 
     auto nextToken = [] (const std::string &line, std::size_t &beginToken) -> std::string  {
-        auto endToken = line.find(StorageSerializer::fieldSeparator(), beginToken);
+        auto endToken = line.find(PathConfig::StoragePath::fieldSeparator, beginToken);
         if (endToken != std::string::npos) {
             auto token = line.substr(beginToken, endToken - beginToken);
             beginToken = endToken + 1;
@@ -57,7 +57,7 @@ CynaraAdminPolicies parse(const std::shared_ptr<std::istream> &input,
 
     for (std::size_t lineNum = 1; !input->eof(); ++lineNum) {
         std::string line;
-        std::getline(*input, line, StorageSerializer::recordSeparator());
+        std::getline(*input, line, PathConfig::StoragePath::recordSeparator);
 
         if (line.empty())
             break;

src/storage/BucketDeserializer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ * Copyright (c) 2014-2015 Samsung Electronics Co., Ltd All Rights Reserved
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -25,13 +25,13 @@
 #include <string>
 #include <vector>
 
+#include <config/PathConfig.h>
 #include <exceptions/BucketRecordCorruptedException.h>
 #include <types/PolicyCollection.h>
 #include <types/PolicyResult.h>
 #include <types/PolicyType.h>
 
 #include <storage/StorageDeserializer.h>
-#include <storage/StorageSerializer.h>
 
 #include "BucketDeserializer.h"
 
@@ -43,7 +43,7 @@ PolicyCollection BucketDeserializer::loadPolicies(void) {
     // TODO: Get someone smart to do error checking on stream
     for (std::size_t lineNum = 1; !m_inStream->eof(); ++lineNum) {
         std::string line;
-        std::getline(*m_inStream, line, StorageSerializer::recordSeparator());
+        std::getline(*m_inStream, line, PathConfig::StoragePath::recordSeparator);
 
         if (line.empty())
             break;
@@ -67,7 +67,7 @@ PolicyKey BucketDeserializer::parseKey(const std::string &line, std::size_t &beg
     std::array<std::string, 3> keyFeatures;
 
     for (std::size_t tokenNum = 0; tokenNum < keyFeatures.size(); ++tokenNum) {
-        auto endToken = line.find(StorageSerializer::fieldSeparator(), beginToken);
+        auto endToken = line.find(PathConfig::StoragePath::fieldSeparator, beginToken);
         if (endToken != std::string::npos) {
             keyFeatures[tokenNum] = line.substr(beginToken, endToken - beginToken);
             beginToken = endToken + 1;

src/storage/StorageDeserializer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ * Copyright (c) 2014-2015 Samsung Electronics Co., Ltd All Rights Reserved
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -24,13 +24,13 @@
 #include <memory>
 #include <string>
 
+#include <config/PathConfig.h>
 #include <exceptions/BucketDeserializationException.h>
 #include <exceptions/BucketRecordCorruptedException.h>
 #include <types/PolicyType.h>
 
 #include <storage/BucketDeserializer.h>
 #include <storage/Buckets.h>
-#include <storage/StorageSerializer.h>
 
 #include "StorageDeserializer.h"
 
@@ -46,7 +46,7 @@ void StorageDeserializer::initBuckets(Buckets &buckets) {
 
     for (std::size_t lineNum = 1; !m_inStream->eof(); ++lineNum) {
         std::string line;
-        std::getline(*m_inStream, line, StorageSerializer::recordSeparator());
+        std::getline(*m_inStream, line, PathConfig::StoragePath::recordSeparator);
 
         if (line.empty())
             break;
@@ -80,7 +80,7 @@ void StorageDeserializer::loadBuckets(Buckets &buckets) {
 
 PolicyBucketId StorageDeserializer::parseBucketId(const std::string &line,
                                                     std::size_t &beginToken) {
-    auto bucketNameEndToken = line.find(StorageSerializer::fieldSeparator(), beginToken);
+    auto bucketNameEndToken = line.find(PathConfig::StoragePath::fieldSeparator, beginToken);
     if (bucketNameEndToken != std::string::npos) {
         auto bucketName = line.substr(beginToken, bucketNameEndToken - beginToken);
         beginToken = bucketNameEndToken + 1;

src/storage/StorageSerializer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ * Copyright (c) 2014-2015 Samsung Electronics Co., Ltd All Rights Reserved
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@
 #include <fstream>
 #include <memory>
 
+#include <config/PathConfig.h>
 #include <types/PolicyBucket.h>
 #include <types/PolicyBucketId.h>
 #include <types/PolicyCollection.h>
@@ -56,13 +57,13 @@ protected:
     inline void dumpFields(const Arg1 &arg1, const Args&... args) {
         dump(arg1);
         if (sizeof...(Args) > 0) {
-            *m_outStream << fieldSeparator();
+            *m_outStream << PathConfig::StoragePath::fieldSeparator;
         }
         dumpFields(args...);
     }
 
     inline void dumpFields(void) {
-        *m_outStream << recordSeparator();
+        *m_outStream << PathConfig::StoragePath::recordSeparator;
     }
 
     void dump(const PolicyKeyFeature &keyFeature);
@@ -72,18 +73,6 @@ protected:
 
 private:
     std::shared_ptr<std::ostream> m_outStream;
-
-    static char m_fieldSeparator;
-    static char m_recordSeparator;
-
-public:
-    static const char &fieldSeparator(void) {
-        return m_fieldSeparator;
-    }
-
-    static const char &recordSeparator(void) {
-        return m_recordSeparator;
-    }
 };
 
 } /* namespace Cynara */
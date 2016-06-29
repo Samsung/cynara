9d10d20e701e461d2a2373368167a642c288b93d
Oskar Świtalski <o.switalski@samsung.com>
2016-06-29 10:47:41 +0200

    Add catching exceptions at Cyad main function
    Change-Id: I4ad9e190e384c7f801173b85213636ee4a32d156
    Signed-off-by: Oskar Świtalski <o.switalski@samsung.com>

--- src/cyad/main.cpp
+++ src/cyad/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ * Copyright (c) 2014-2016 Samsung Electronics Co., Ltd All Rights Reserved
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -16,13 +16,28 @@
 /**
  * @file        src/cyad/main.cpp
  * @author      Aleksander Zdyb <a.zdyb@samsung.com>
+ * @author      Oskar Świtalski <o.switalski@samsung.com>
  * @version     1.0
  * @brief       A command-line tool for managing Cynara's database
  */
 
+#include <exception>
+#include <iostream>
+
 #include <cyad/Cyad.h>
+#include <cynara-error.h>
 
 int main(int argc, char **argv) {
-    Cynara::Cyad cyad(argc, argv);
-    return cyad.run();
+    int result = CYNARA_API_UNKNOWN_ERROR;
+
+    try {
+        Cynara::Cyad cyad(argc, argv);
+        result = cyad.run();
+    } catch (const std::exception &e) {
+        std::cerr << "Caught exception: " << e.what() << std::endl;
+    } catch (...) {
+        std::cerr << "Caught unknow exception" << std::endl;
+    }
+
+    return result;
 }
